b7329be2d271fc3353d476da6a1a388f79656d2d
Ulysse Boureau <ulysse.boureau@student-cs.fr>
2023-03-21 23:09:41 +0100

    write unet model and create a train file and directory

---
+++ models/unet.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+
+class Squeeze_Excite(nn.Module):
+
+    def __init__(self,channel,reduction):
+        super().__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self,x):
+        b, c, _, _ = x.size()
+        y = self.avgpool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+class VGGBlock(nn.Module):
+
+    def __init__(self, in_channels, middle_channels, out_channels):
+        super().__init__()
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_channels, middle_channels, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(middle_channels)
+        self.conv2 = nn.Conv2d(middle_channels, out_channels, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.SE = Squeeze_Excite(out_channels,8)
+
+    def forward(self,x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.SE(out)
+
+        return(out)
+
+class UNet(nn.Module):
+
+    def __init__(self, num_classes, input_channels=3, **kwargs):
+        super().__init__()
+
+        nb_filter = [32, 64, 128, 256, 512]
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv0_0 = VGGBlock(input_channels, nb_filter[0], nb_filter[0])
+        self.conv1_0 = VGGBlock(nb_filter[0], nb_filter[1], nb_filter[1])
+        self.conv2_0 = VGGBlock(nb_filter[1], nb_filter[2], nb_filter[2])
+        self.conv3_0 = VGGBlock(nb_filter[2], nb_filter[3], nb_filter[3])
+        self.conv4_0 = VGGBlock(nb_filter[3], nb_filter[4], nb_filter[4])
+
+        self.conv3_1 = VGGBlock(nb_filter[3]+nb_filter[4], nb_filter[3], nb_filter[3])
+        self.conv2_2 = VGGBlock(nb_filter[2]+nb_filter[3], nb_filter[2], nb_filter[2])
+        self.conv1_3 = VGGBlock(nb_filter[1]+nb_filter[2], nb_filter[1], nb_filter[1])
+        self.conv0_4 = VGGBlock(nb_filter[0]+nb_filter[1], nb_filter[0], nb_filter[0])
+
+        self.final = nn.Conv2d(nb_filter[0], num_classes, kernel_size=1)
+
+    def forward(self, input):
+        x0_0 = self.conv0_0(input)
+        x1_0 = self.conv1_0(self.pool(x0_0))
+        x2_0 = self.conv2_0(self.pool(x1_0))
+        x3_0 = self.conv3_0(self.pool(x2_0))
+        x4_0 = self.conv4_0(self.pool(x3_0))
+
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
+        x2_2 = self.conv2_2(torch.cat([x2_0, self.up(x3_1)], 1))
+        x1_3 = self.conv1_3(torch.cat([x1_0, self.up(x2_2)], 1))
+        x0_4 = self.conv0_4(torch.cat([x0_0, self.up(x1_3)], 1))
+
+        output = self.final(x0_4)
+        return output
---
+++ train/train.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import time
+import numpy as np
+
+def main_train(model, loss_fn, optimizer, n_epochs, dataloader, device) -> None:
+    # Put model in train mode
+    model.train()
+    n = len(dataloader)
+
+    start_time_global = time.time()
+
+    # Loop through data loader data batches
+    loss_list = np.zeros(n_epochs)
+    for epoch in range(n_epochs):
+
+        start_time_epoch = time.time()
+
+        train_loss = 0
+
+        #############################################
+        #for X, y in TRAIN_DATALOADER:
+        for X, y in dataloader:
+            # 1. Forward pass
+            model_output = model(X.to(device))
+
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(model_output,y)
+            train_loss += loss.item()
+
+            # 3. Optimizer zero grad
+            optimizer.zero_grad()
+
+            # 4. Loss backward
+            loss.backward()
+
+            # 5. Optimizer step
+            optimizer.step()
+
+        loss_list[epoch] = train_loss
+        print(
+            f"epoch {epoch+1}/{n_epochs},"
+            f" train_loss = {train_loss/n:.2e},"
+            #f" validation_loss = {test_model(model, loss_fn):.2e},"
+            f" time spent during this epoch = {time.time() - start_time_epoch:.2f}s,"
+            f" total time spent = {time.time() - start_time_global:.2f}s"
+        )
+
+    return loss_list
